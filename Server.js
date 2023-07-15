c72c0cb10f18e986b29ad78c2c6d15b7873aebff
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-07-15 17:34:10 +0530

    Create Req/Res

--- Server.js
+++ Server.js
@@ -1,9 +1,23 @@
 const http=require('http')
 
 const server=http.createServer((req,res)=>{
-  console.log("utkarsh badoni")
-  res.write('<h1>Utkarsh badoni</h1>')
-  res.end();
+  const url=req.url;
+  console.log(url);
+  if(url==='/Home'){
+    console.log("Welcome Home")
+    res.write('<h1>Welcome Home</h1>')
+    return res.end();
+  }
+  if(url==='/About'){
+    console.log("Welcome to About Us Page")
+    res.write('<h1>Welcome to About Us Page</h1>')
+    return res.end();
+  }
+  if(url==='/Node'){
+    console.log("Welcome to my Node js Project")
+    res.write('<h1>Welcome to my Node js Project</h1>')
+    return res.end();
+  }
 })
 
-server.listen(4000) 
+server.listen(3000) 
