5e05df61e1cbb094e6c137e43996ef3880bd2e26
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-07-21 03:51:16 +0530

    2 Middlerware

--- ExportContent.js
+++ ExportContent.js
@@ -1,2 +1,12 @@
-const receive=require('./Server')
-console.log(receive.next)
+const http=require('http')
+const express=require('express')
+const app=express();
+app.use((req,res,next)=>{
+  console.log('In the middleware')
+  next();
+})
+app.use((req,res,next)=>{
+    console.log('In another middleware')
+})
+const server=http.createServer(app);
+server.listen(3000) 
